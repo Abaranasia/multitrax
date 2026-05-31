32453fb0717f652e5528d16986b196ac287318aa
José Luis Sanchez Rodríguez <joseluis.sanchez@icareweb.com>
2026-05-31 11:44:45 +0200

    chore: fix Canvas import

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AudioProvider } from './context/AudioContext';
-import { Canvas } from './components/Canvas';
+import { Canvas } from './components/Canvas/Canvas';
 import './index.css';
 
 const root = createRoot(document.getElementById('root')!);

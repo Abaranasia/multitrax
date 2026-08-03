146deaec2817af4a8abaf47183e22045aa741bef
Abaranasia <rankirlian@gmail.com>
2026-08-03 18:40:34 +0200

    feat: add ChannelStrip component

---
+++ src/renderer/components/MixerView/ChannelStrip.tsx
@@ -0,0 +1,105 @@
+import { TrackEntry } from '../../context/audioContextInstance';
+import { formatDb } from '../../utils/formatDb';
+import { useChannelStrip } from './useChannelStrip';
+import { PanDial } from './PanDial';
+import {
+  EffectDialogs,
+  WaveformCanvas,
+  useWaveformCanvas,
+  VolumeControl,
+  useVolumeControl,
+  usePanControl,
+  EffectToggles,
+  useEffectToggles,
+  PlaybackButtons,
+  TransportToggles,
+  useTransportControls,
+  useFilterSettingsDialog,
+  useDistortionSettingsDialog,
+  useFadeSettingsDialog,
+  useDelaySettingsDialog,
+  useReverbSettingsDialog,
+} from '../TrackPlayer/components';
+
+import './MixerView.css';
+
+interface ChannelStripProps {
+  track: TrackEntry;
+}
+
+export const ChannelStrip = ({ track }: ChannelStripProps) => {
+  const { state } = track;
+  const {
+    fmt,
+    onProgressClick,
+    progress,
+    play,
+    pause,
+    stop,
+    setLoop,
+    setFadeIn,
+    setFadeOut,
+    setSeekFade,
+    setVolume,
+    setPan,
+  } = useChannelStrip(state);
+
+  const waveformCanvasRef = useWaveformCanvas(state.waveform, progress);
+  const volumeControl = useVolumeControl(state, setVolume);
+  const panControl = usePanControl(state, setPan);
+  const filterDialog = useFilterSettingsDialog(state);
+  const distortionDialog = useDistortionSettingsDialog(state);
+  const fadeDialog = useFadeSettingsDialog(state);
+  const delayDialog = useDelaySettingsDialog(state);
+  const reverbDialog = useReverbSettingsDialog(state);
+  const effectToggles = useEffectToggles(state, {
+    onFilterOpen: filterDialog.open,
+    onDistortionOpen: distortionDialog.open,
+    onDelayOpen: delayDialog.open,
+    onReverbOpen: reverbDialog.open,
+  });
+  const transportControls = useTransportControls(
+    state,
+    { play, pause, stop, setLoop, setFadeIn, setFadeOut, setSeekFade, onOpenFadeSettings: fadeDialog.open },
+    fmt,
+  );
+
+  return (
+    <div className="mixer-strip">
+      <div className="mixer-strip-title" title={state.title}>
+        {state.title.split(/[\\/]/).pop() ?? state.title}
+      </div>
+
+      <WaveformCanvas
+        canvasRef={waveformCanvasRef}
+        progress={progress}
+        title={state.title}
+        onProgressClick={onProgressClick}
+      />
+
+      <EffectToggles {...effectToggles} />
+
+      <PanDial {...panControl} />
+
+      <div className="mixer-middle-row">
+        <TransportToggles {...transportControls} />
+
+        <div className="mixer-fader">
+          <VolumeControl {...volumeControl} />
+        </div>
+      </div>
+
+      <div className="mixer-db">{formatDb(state.volume)}</div>
+
+      <PlaybackButtons {...transportControls} />
+
+      <EffectDialogs
+        filterDialog={filterDialog}
+        distortionDialog={distortionDialog}
+        fadeDialog={fadeDialog}
+        delayDialog={delayDialog}
+        reverbDialog={reverbDialog}
+      />
+    </div>
+  );
+};
